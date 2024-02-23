dcb2748180c4be6c3cd6453ada82d2c2c2560791
kaci.kechadi1@gmail.com <kaci.kechadi1@gmail.com>
2024-02-23 13:05:45 +0000

    Updated recipe GIT_FOR_PROJECTS_FOR_KACI_PRUNE_FLOW.recipe_from_notebook_test_git

--- recipes/recipe_from_notebook_test_git.py
+++ recipes/recipe_from_notebook_test_git.py
@@ -4,11 +4,4 @@ import dataiku
 from dataiku import pandasutils as pdu
 import pandas as pd
 
-# Example: load a DSS dataset as a Pandas dataframe
-mydataset = dataiku.Dataset("mydataset")
-mydataset_df = mydataset.get_dataframe()
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Recipe outputs
-test_output = dataiku.Dataset("test_output")
-test_output.write_with_schema(pandas_dataframe)
+print("Hello Dataiku!")
